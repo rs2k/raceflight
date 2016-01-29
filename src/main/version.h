dc82031c387ed830c1894f3ed6637b74ce3b9d0f
rs2k <kalyn.doerr@gmail.com>
2016-01-29 16:17:41 -0600

    Version update.

--- src/main/version.h
+++ src/main/version.h
@@ -66,10 +66,10 @@
 
 #define FC_VERSION_MAJOR            16  // build year
 #define FC_VERSION_MINOR            01  // build month
-#define FC_VERSION_PATCH_LEVEL      28  // build day
-#define FC_VERSION_LETTER           "d" // build of day
+#define FC_VERSION_PATCH_LEVEL      29  // build day
+#define FC_VERSION_LETTER           "a" // build of day
 
-#define FC_VERSION_COMMENT          "RaceFlight Release 1 RC3a"
+#define FC_VERSION_COMMENT          "RaceFlight Release 1 RC4"
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
