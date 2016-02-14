6e3c35a42a1ee8d6606398971d207fe64332df5a
Michael Corcoran <mrc93@uclive.ac.nz>
2016-02-14 23:00:29 +1300

    USB: F1/F3: Send custom USB descriptor strings

--- src/main/vcp/hw_config.c
+++ src/main/vcp/hw_config.c
@@ -53,7 +53,6 @@ extern __IO uint32_t receiveLength;                          // HJI
 
 uint8_t receiveBuffer[64];                                   // HJI
 uint32_t sendLength;                                          // HJI
-static void IntToUnicode(uint32_t value, uint8_t *pbuf, uint8_t len);
 /* Extern variables ----------------------------------------------------------*/
 
 /* Private function prototypes -----------------------------------------------*/
@@ -226,53 +225,6 @@ void USB_Interrupts_Config(void)
 void USB_Cable_Config(FunctionalState NewState)
 {
     UNUSED(NewState);
-}
-
-/*******************************************************************************
- * Function Name  : Get_SerialNum.
- * Description    : Create the serial number string descriptor.
- * Input          : None.
- * Output         : None.
- * Return         : None.
- *******************************************************************************/
-void Get_SerialNum(void)
-{
-    uint32_t Device_Serial0, Device_Serial1, Device_Serial2;
-
-    Device_Serial0 = *(uint32_t*)ID1;
-    Device_Serial1 = *(uint32_t*)ID2;
-    Device_Serial2 = *(uint32_t*)ID3;
-
-    Device_Serial0 += Device_Serial2;
-
-    if (Device_Serial0 != 0) {
-        IntToUnicode(Device_Serial0, &Virtual_Com_Port_StringSerial[2], 8);
-        IntToUnicode(Device_Serial1, &Virtual_Com_Port_StringSerial[18], 4);
-    }
-}
-
-/*******************************************************************************
- * Function Name  : HexToChar.
- * Description    : Convert Hex 32Bits value into char.
- * Input          : None.
- * Output         : None.
- * Return         : None.
- *******************************************************************************/
-static void IntToUnicode(uint32_t value, uint8_t *pbuf, uint8_t len)
-{
-    uint8_t idx = 0;
-
-    for (idx = 0; idx < len; idx++) {
-        if (((value >> 28)) < 0xA) {
-            pbuf[2 * idx] = (value >> 28) + '0';
-        } else {
-            pbuf[2 * idx] = (value >> 28) + 'A' - 10;
-        }
-
-        value = value << 4;
-
-        pbuf[2 * idx + 1] = 0;
-    }
 }
 
 /*******************************************************************************
