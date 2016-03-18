a5a70d6e10b537375c3b6135af6f28732c467bd9
rs2k <kalyn.doerr@gmail.com>
2016-03-18 02:20:21 -0500

    Up versions, add Sparky 2 and Sparky back to top_makefile

--- src/main/version.h
+++ src/main/version.h
@@ -17,10 +17,10 @@
 
 #define FC_VERSION_MAJOR            16  // build year
 #define FC_VERSION_MINOR            03  // build month
-#define FC_VERSION_PATCH_LEVEL      15  // build day
+#define FC_VERSION_PATCH_LEVEL      18  // build day
 #define FC_VERSION_LETTER           "a" // build of day
 
-#define FC_VERSION_COMMENT          "RaceFlight Release 1 RC6.2 - New filters"
+#define FC_VERSION_COMMENT          "RaceFlight Next - Beta"
 
 
 // Example of __DATE__ string: "Jul 27 2012"
