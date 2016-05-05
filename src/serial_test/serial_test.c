2234ed2ec1a2a97918b04c78387e08eeabf0cc3e
Preston Garrison <git@prestongarrison.com>
2016-05-05 14:17:09 -0700

    Serial test

---
+++ src/serial_test/serial_test.c
@@ -0,0 +1,107 @@
+//
+// serial.c / serial.cpp
+// A simple serial port writing example
+// Written by Ted Burke - last updated 13-2-2013
+//
+// To compile with MinGW:
+//
+//      gcc -o serial.exe serial.c
+//
+// To compile with cl, the Microsoft compiler:
+//
+//      cl serial.cpp
+//
+// To run:
+//
+//      serial.exe
+//
+ 
+#include <windows.h>
+#include <stdio.h>
+ 
+int main()
+{
+    // Define the five bytes to send ("hello")
+    char bytes_to_send[5];
+    bytes_to_send[0] = 104;
+    bytes_to_send[1] = 101;
+    bytes_to_send[2] = 108;
+    bytes_to_send[3] = 108;
+    bytes_to_send[4] = 111;
+ 
+
+    // Declare variables and structures
+    HANDLE hSerial;
+    DCB dcbSerialParams = {0};
+    COMMTIMEOUTS timeouts = {0};
+         
+    // Open the highest available serial port number
+    fprintf(stderr, "Opening serial port...");
+    hSerial = CreateFile(
+                "\\\\.\\COM22", GENERIC_READ|GENERIC_WRITE, 0, NULL,
+                OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL );
+    if (hSerial == INVALID_HANDLE_VALUE)
+    {
+            fprintf(stderr, "Error\n");
+            return 1;
+    }
+    else fprintf(stderr, "OK\n");
+     
+    // Set device parameters (38400 baud, 1 start bit,
+    // 1 stop bit, no parity)
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+    if (GetCommState(hSerial, &dcbSerialParams) == 0)
+    {
+        fprintf(stderr, "Error getting device state\n");
+        CloseHandle(hSerial);
+        return 1;
+    }
+     
+    dcbSerialParams.BaudRate = CBR_115200;
+    dcbSerialParams.ByteSize = 8;
+    dcbSerialParams.StopBits = ONESTOPBIT;
+    dcbSerialParams.Parity = NOPARITY;
+    if(SetCommState(hSerial, &dcbSerialParams) == 0)
+    {
+        fprintf(stderr, "Error setting device parameters\n");
+        CloseHandle(hSerial);
+        return 1;
+    }
+ 
+    // Set COM port timeout settings
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    timeouts.WriteTotalTimeoutConstant = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+    if(SetCommTimeouts(hSerial, &timeouts) == 0)
+    {
+        fprintf(stderr, "Error setting timeouts\n");
+        CloseHandle(hSerial);
+        return 1;
+    }
+ 
+    // Send specified text (remaining command line arguments)
+    DWORD bytes_written, total_bytes_written = 0;
+    fprintf(stderr, "Sending bytes...");
+    if(!WriteFile(hSerial, bytes_to_send, 5, &bytes_written, NULL))
+    {
+        fprintf(stderr, "Error\n");
+        CloseHandle(hSerial);
+        return 1;
+    }   
+    fprintf(stderr, "%d bytes written\n", bytes_written);
+     
+    // Close serial port
+    fprintf(stderr, "Closing serial port...");
+    if (CloseHandle(hSerial) == 0)
+    {
+        fprintf(stderr, "Error\n");
+        return 1;
+    }
+    fprintf(stderr, "OK\n");
+ 
+    // exit normally
+    return 0;
+}
+
